eac5ea3a189d50868e4f42988998bb95596536b5
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-19 09:30:12 -0800

    extract more utilities

--- src/utilities/exampleUtility.ts
+++ src/utilities/exampleUtility.ts
@@ -1,10 +1,9 @@
-import { from, Power, Scalar, to } from '@musical-patterns/utilities'
-import { applyPower } from '../../../../src'
+import { apply, from, Power, Scalar, to } from '@musical-patterns/utilities'
 import { EXAMPLE_CONSTANT } from './constants'
 
 const exampleUtility: (power: Power) => Scalar =
     (power: Power): Scalar =>
-        to.Scalar(applyPower(from.Base(EXAMPLE_CONSTANT), power))
+        to.Scalar(apply.Power(from.Base(EXAMPLE_CONSTANT), power))
 
 export {
     exampleUtility,
