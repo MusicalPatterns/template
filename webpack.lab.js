6ffb6fc7d008588e8883c43a1616ace113065d6a
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-24 17:47:40 -0800

    1.0.70: ignorefilessharedbyallservices

--- webpack.lab.js
+++
@@ -1,6 +0,0 @@
-const browser = require('./webpack.browser')
-const merge = require('webpack-merge')
-
-module.exports = merge(browser, {
-    mode: 'production',
-})
--- webpack.local.js
+++
@@ -1,7 +0,0 @@
-const library = require('./webpack.library')
-const merge = require('webpack-merge')
-
-module.exports = merge(library, {
-    mode: 'development',
-    devtool: 'eval-source-map',
-})
