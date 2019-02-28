e46700ed62438db55e67834b9be6332f3327e82a
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-28 15:46:53 -0800

    1.0.88: include pattern version

--- src/patterns.ts
+++ src/patterns.ts
@@ -14,6 +14,7 @@ const metadata: Metadata = {
     mostRecentPublish: process.env.PUBLISH_DATE || '2018-10-31T07:00:00.000Z',
     musicalIdeaIllustrated: '',
     originalPublish: '2018-10-31T07:00:00.000Z',
+    version: process.env.PATTERN_VERSION || 'unknown',
 }
 
 const pattern: Pattern<StandardSpec> = {
