8aa75de1b2f3934ed3dbf75aa9704ccc790acdd7
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-15 12:45:29 -0700

    1.0.104: load post in any env other than test - turns out node env is undefined in dev, not dev

--- src/metadata/posts.ts
+++ src/metadata/posts.ts
@@ -3,7 +3,7 @@ import { Maybe } from '@musical-patterns/utilities'
 const env: Maybe<string> = process.env.NODE_ENV
 
 // tslint:disable-next-line
-const post: string = env === 'development' || env === 'production' ? require('./post.html') : ''
+const post: string = env !== 'test' ? require('./post.html') : ''
 
 export {
     post,
