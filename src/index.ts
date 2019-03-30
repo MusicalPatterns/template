317a40b8349d5345db22170a61520fcd3ceea2a9
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-29 21:15:32 -0700

    1.0.107: exclude snapshot from package

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
 export { pattern, patterns } from './patterns'
-
-// tslint:disable-next-line no-default-import
-import snapshot from './snapshot.json'
-
-export { snapshot }
--- src/indexForTest.ts
+++ src/indexForTest.ts
@@ -1,7 +1,2 @@
 // tslint:disable no-reaching-imports
 export { pattern } from './patterns'
-
-// tslint:disable-next-line no-default-import
-import * as snapshot from './snapshot.json'
-
-export { snapshot }
