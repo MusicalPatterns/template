b32fcaa76a1300988423a4be30be6c4fcffb7a5c
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-03-01 11:30:37 -0800

    1.0.138: midi

--- src/metadata/posts.ts
+++ src/metadata/posts.ts
@@ -3,7 +3,7 @@ import { Maybe } from '@musical-patterns/utilities'
 const env: Maybe<string> = process.env.NODE_ENV
 
 // tslint:disable-next-line
-const post: string = env !== 'test' && env != 'snapshots' ? require('./post.html') : ''
+const post: string = env !== 'test' && env != 'material' ? require('./post.html') : ''
 
 export {
     post,
